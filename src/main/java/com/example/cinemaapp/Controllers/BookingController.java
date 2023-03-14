edcd19babbe33d977c4fc8f373d02634ba64f3fd
Evangelos Karachristos <karachristos.e@outlook.com>
2023-03-14 12:29:23 +0200

    remove some credentials

--- src/main/java/com/example/cinemaapp/Controllers/BookingController.java
+++ src/main/java/com/example/cinemaapp/Controllers/BookingController.java
@@ -1,9 +1,7 @@
 package com.example.cinemaapp.Controllers;
 
 import com.example.cinemaapp.Model.TicketsBooking;
-import com.example.cinemaapp.Repository.CurrentUserSessionRepo;
 import com.example.cinemaapp.Service.BookingService;
-import com.example.cinemaapp.Service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,26 +10,26 @@ import java.util.List;
 @RestController
 public class BookingController {
     @Autowired
-    private BookingService service;
+    private BookingService bookingService;
 
     @GetMapping("/bookings")
     public List<TicketsBooking> getAllBookings(){
-        return service.showAllBooking();
+        return bookingService.showAllBooking();
     }
 
     @PostMapping("/booking")
     public TicketsBooking addBooking(@RequestBody TicketsBooking booking){
 
-        return service.addBooking(booking);
+        return bookingService.addBooking(booking);
     }
 
     @PutMapping("/booking")
     public TicketsBooking updateBooking(@RequestBody TicketsBooking booking){
-        return service.updateBooking(booking);
+        return bookingService.updateBooking(booking);
     }
 
     @DeleteMapping("/booking")
     public TicketsBooking deleteBooking(@RequestBody TicketsBooking booking){
-        return service.cancelBooking(booking);
+        return bookingService.cancelBooking(booking);
     }
 }
